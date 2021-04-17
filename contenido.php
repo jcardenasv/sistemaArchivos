cd7615f24a2e825a02b69ec3d56648005f57740a
SaraBalbin <51859641+SaraBalbin@users.noreply.github.com>
2021-04-17 14:51:07 -0500

    Inicio botones

--- contenido.php
+++ contenido.php
@@ -1,6 +1,44 @@
 <?php
 //      /var/www/html/sistemaArchivos$ git commit -a -m "Alto cambio"
+//      /var/www/html/sistemaArchivos$ git add --all
 
-echo "Hola gente <br> Se les quiere";
+
+echo '
+<!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+';
+include 'estructura.php'
 
 ?>
+
+<html>
+
+<head>
+    <title> Sistema </title>
+</head>
+
+<body>
+    <nav class="navbar navbar-light bg-light">
+        <form class="form-inline">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span type ="button" class="btn btn-outline-dark" id="basic-addon1"> Devolver </span>
+                </div>
+                <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+            </div>
+        </form>
+        <button type="button" class="btn btn-outline-dark" data-toggle="modal" data-target="#modalRenombrar"> Renombrar </button>
+
+
+    
+    </nav>
+
+</body>
+
+</html>
---
+++ estructura.php
@@ -0,0 +1,5 @@
+<?php
+//      /var/www/html/sistemaArchivos$ git commit -a -m "Alto cambio"
+
+
+?>
